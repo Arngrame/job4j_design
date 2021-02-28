2c5d846fa5c23efed9cc9b527f87fa527f49f3ac
Arngrame <arngrame.dev@gmail.com>
2021-03-01 02:36:57 +0400

    Added tests for increase coverage

--- src/test/java/ru/job4j/it/MatrixItTest.java
+++ src/test/java/ru/job4j/it/MatrixItTest.java
@@ -10,6 +10,48 @@ import static org.junit.Assert.assertThat;
 public class MatrixItTest {
 
     @Test
+    public void testMatrix() {
+        int[][] in = {
+                {1, 2},
+                {3, 4}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void testMatrixWithEmptyValues() {
+        int[][] in = {
+                {1, 2},
+                {3, 4},
+                {},
+                {5, 8}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
     public void when4El() {
         int[][] in = {
                 {1}
