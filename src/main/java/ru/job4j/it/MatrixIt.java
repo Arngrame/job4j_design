7973ddd625ad17f5f5ff41ca1efde7a8b3f210cb
Arngrame <arngrame.dev@gmail.com>
2021-03-01 20:19:30 +0400

    Small fixes

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -8,7 +8,6 @@ public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
     private int row = 0;
     private int column = 0;
-    private boolean goNextRow = false;
 
     public MatrixIt(int[][] data) {
         this.data = data;
@@ -22,26 +21,19 @@ public class MatrixIt implements Iterator<Integer> {
             // check column index less than current row length
             if (column < data[row].length) {
                 return true;
-            } else if (column == data[row].length && data[row].length != 0) { // check breakline is needed if column index equals current row length
-                // check if next line exists
-                if (row + 1 == data.length) {
-                    return false;
-                }
-                row++;
-                column = 0;
-                return true;
-            } else if (data[row].length == 0) { //current row length = 0
-                while (data[row].length == 0) { // check next rows
-                    row++;
-                    if (row == data.length) { // reach max row?
-                        return false;
-                    }
-                }
+            }
 
-                if (data[row].length != 0) {
+            // in other cases row should be increased and column should set 0 due to new row started
+            column = 0;
+            row++;
+
+            // increase row index until reach row border
+            while (row != data.length) {
+                if (data[row].length == 0) {
+                    row++;
+                } else {
                     return true;
                 }
-
             }
         }
         return false;
@@ -51,12 +43,6 @@ public class MatrixIt implements Iterator<Integer> {
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
-        }
-
-        if (data[row].length == 0) {
-            while (data[row].length == 0) {
-                row++;
-            }
         }
 
         return data[row][column++];
