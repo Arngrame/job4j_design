90e97584b7594064295853897812be1491a8ffc8
Arngrame <arngrame.dev@gmail.com>
2021-03-04 02:22:38 +0400

    Store added

---
+++ src/main/java/ru/job4j/store/MemStore.java
@@ -0,0 +1,42 @@
+package ru.job4j.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final List<T> mem = new ArrayList<>();
+
+    @Override
+    public void add(T model) {
+        mem.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        int index = findIndexById(id);
+        mem.set(index, model);
+        return true;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        int index = findIndexById(id);
+        mem.remove(index);
+        return true;
+    }
+
+    @Override
+    public T findById(String id) {
+        return mem.get(findIndexById(id));
+    }
+
+    private int findIndexById(String id) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i) != null && id.equals(mem.get(i).getId())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
