d3d46be230778a82e33adbe0049078eee74be534
Arngrame <arngrame.dev@gmail.com>
2021-03-29 03:17:55 +0400

    #482113 Only #equals overriden

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -23,11 +23,23 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        int defaultHash = 31;
-        defaultHash = defaultHash * 17 + (name == null ? 0 : name.hashCode());
-        defaultHash = defaultHash * 17 + children;
-        defaultHash = defaultHash * 17 + (birthday == null ? 0 : birthday.hashCode());
-        return defaultHash;
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && name.equals(user.name) && birthday.equals(user.birthday);
     }
+
+    //    @Override
+//    public int hashCode() {
+//        int defaultHash = 31;
+//        defaultHash = defaultHash * 17 + (name == null ? 0 : name.hashCode());
+//        defaultHash = defaultHash * 17 + children;
+//        defaultHash = defaultHash * 17 + (birthday == null ? 0 : birthday.hashCode());
+//        return defaultHash;
+//    }
 }
