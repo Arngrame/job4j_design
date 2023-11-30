aaced0973fa7ae00a91396c9a46e76b45413b6c3
Arngrame <arngrame.dev@gmail.com>
2023-11-30 23:07:21 +0400

    Cyclic iterator ready to review

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private List<T> data;
+    private int index;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+        index = -1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data == null || data.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public T next() {
+        if (hasNext()) {
+            index = index == data.size() - 1 ? 0 : index + 1;
+            return data.get(index);
+        }
+        throw new NoSuchElementException();
+    }
+}
