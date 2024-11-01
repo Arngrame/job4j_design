2224ec8615a72833fbb5bbcb39f15c6310380f8e
Arngrame <arngrame.dev@gmail.com>
2024-11-01 23:24:03 +0400

    Tree Task#2

--- src/test/java/ru/job4j/chapter01/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/chapter01/tree/SimpleTreeTest.java
@@ -34,4 +34,42 @@ public class SimpleTreeTest {
         tree.add(5, 6);
         assertThat(tree.add(2, 6)).isFalse();
     }
+
+    @Test
+    void whenRootIsNotBinary () {
+        Tree<Integer> tree = new SimpleTree<>(1);
+
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+
+        assertThat(tree.isBinary()).isFalse();
+    }
+
+    @Test
+    void whenOneOfChildIsNotBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 10);
+        tree.add(2, 20);
+        tree.add(2, 30);
+
+        assertThat(tree.isBinary()).isFalse();
+    }
+
+    @Test
+    void whenTreeIsBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 10);
+        tree.add(2, 20);
+        tree.add(3, 30);
+        tree.add(3, 40);
+
+        assertThat(tree.isBinary()).isTrue();
+    }
 }
